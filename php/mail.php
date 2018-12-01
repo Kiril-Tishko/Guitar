4a4d9a766b8077e329cd47d716d30553aef0d3ee
Kiril-Tishko <Kiril-Tishko@users.noreply.github.com>
2018-12-02 00:52:39 +0200

    animation

--- php/mail.php
+++ php/mail.php
@@ -13,19 +13,13 @@ $text = $_POST['user_text'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'kstishko@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'nonapetona'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'kstishko@gmail.com';
+$mail->Password = 'Password';
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587; // TCP port to connect to / этот порт может отличаться у других провайдеров
+$mail->Port = 587; // TCP port to connect to
 
-$mail->setFrom('kstishko@gmail.com'); // от кого будет уходить письмо?
-$mail->addAddress('xunijuv@voltaer.com');     // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->setFrom('kstishko@gmail.com');
+$mail->addAddress('xunijuv@voltaer.com');
 $mail->isHTML(true);                                  // Set email format to HTML
 
 $mail->Subject = 'Заявка с тестового сайта';
