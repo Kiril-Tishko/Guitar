41a29193fe013e7bde1d71c58a29cd881938e18c
Kiril-Tishko <Kiril-Tishko@users.noreply.github.com>
2018-12-27 23:08:44 +0200

    fix email-1

--- php/mail.php
+++ php/mail.php
@@ -8,25 +8,24 @@ $name = $_POST['user_name'];
 $email = $_POST['user_email'];
 $text = $_POST['user_text'];
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+$mail->isSMTP();	// Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';	// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;	// Enable SMTP authentication
+$mail->Username = 'dzharuzov@mail.ru';	// Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '12345';	// email password
+$mail->SMTPSecure = 'ssl';	// Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'kstishko@gmail.com';
-$mail->Password = 'Password';
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587; // TCP port to connect to
-
-$mail->setFrom('kstishko@gmail.com');
-$mail->addAddress('xunijuv@voltaer.com');
-$mail->isHTML(true);                                  // Set email format to HTML
+$mail->setFrom('dzharuzov@mail.ru');
+$mail->isHTML(true);	// Set email format to HTML
 
 $mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . '<br>' .$email '<br>' .$text.;
+$mail->Body    = '' .$name . ' оставил заявку, его email ' .$email. '<br>Сообщение от пользователя: ' .$text;
 $mail->AltBody = '';
 
 if(!$mail->send()) {
     echo 'Error';
+} else {
+    header('location: thank-you.html');
 }
 ?>
